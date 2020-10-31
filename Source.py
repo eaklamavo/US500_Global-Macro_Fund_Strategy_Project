edd71f1bb816df182a408d8ee44ca67419b97b1a
eaklamavo <49913698+eaklamavo@users.noreply.github.com>
2020-10-31 00:02:13 +0000

    Add files via upload
    ISM_PMI Basic

---
+++ Source.py
@@ -0,0 +1,33 @@
+###ISM Manufacturing Index (PMI)####
+
+###1####
+##install quandl
+#!pip install Quandl
+########
+
+##import required libraries##
+import pandas as pd
+import requests
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+%matplotlib inline
+import quandl
+
+###2####
+##call ISM_PMI from quandl
+########
+ism_pmi = quandl.get("ISM/MAN_PMI", authtoken="qHmb8WS585_F9y8sD8fx")
+ism_pmi #quick view
+
+###3####
+###simple line chart
+########
+ism_pmi.plot()
+
+###End####
+
+
